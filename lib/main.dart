21011bc801958007883700ab8c32ff3a15560bf9
safitri02 <safitri1337@gmail.com>
2022-08-16 08:45:48 +0700

    add library

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //hidden debug text
       debugShowCheckedModeBanner: false,
+
       title: 'ITC Profile',
       home: Scaffold(
         backgroundColor: Colors.blue,
@@ -25,9 +27,10 @@ class MyApp extends StatelessWidget {
             children: [
               Image.asset('images/logoputih.png'),
               const Text(
-                'ITC Profile!',
+                'ITC PROFILE!',
                 textAlign: TextAlign.center,
                 style: TextStyle(
+                  fontWeight: FontWeight.bold,
                   color: Color(0xffffffff),
                   fontSize: 30,
                 ),
@@ -36,7 +39,7 @@ class MyApp extends StatelessWidget {
                 height: 20,
               ),
               const Text(
-                'ITC (Introduction To Computer) Profile merupakan study kasus pada divisi mobile development. nah dari studi kasus ini harapannya nanti para peserta yang ada di divisi mobile app dapat belajar lebih dalam mengenai flutter yang dimana basicnya dimuali dari sini.',
+                'ITC (Introduction To Computer) Profile merupakan studi kasus pada divisi mobile development. nah dari studi kasus ini harapannya nanti para peserta yang ada di divisi mobile app dapat belajar lebih dalam mengenai flutter yang dimana basicnya dimuali dari sini.',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 20,
@@ -44,10 +47,12 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height: 30,
+                height: 40,
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushNamed(context, '/profile');
+                },
                 child: Text('Lihat Proflie'.toUpperCase()),
                 style: ElevatedButton.styleFrom(
                   primary: Color(0x247699), //warna button
